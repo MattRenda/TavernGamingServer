8ae9db8b72313ae3787cd42061ab5e6a25e872d1
MatthewRenda <MatthewJRenda@Gmail.com>
2023-06-28 09:50:37 -0700

    updates constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
 const constants = {
-  //clientURL: "https://tkns.gg",
-  //serverURL: "https://tkns-server.herokuapp.com",
-  clientURL: "https://taverngaming.z13.web.core.windows.net/",
+  clientURL: "https://taverngaming.com",
   serverURL: "https://tavern-gaming-server-ffc8707ef1ea.herokuapp.com/",
   //serverURL: "http://localhost:8080",
   //clientURL: "http://localhost:3000",
