47e211c0c1573a82b6c974ce8adaf1048f9cdd67
MatthewRenda <MatthewJRenda@Gmail.com>
2023-06-28 17:36:04 -0700

    cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 const constants = {
-  clientURL: "https://www.taverngaming.com",
-  serverURL: "https://tavern-gaming-server-ffc8707ef1ea.herokuapp.com",
-  //serverURL: "http://localhost:8080",
-  //clientURL: "http://localhost:3000",
+  //clientURL: "https://www.taverngaming.com",
+  //serverURL: "https://tavern-gaming-server-ffc8707ef1ea.herokuapp.com",
+  serverURL: "http://localhost:8080",
+  clientURL: "http://localhost:3000",
 };
 
 module.exports = { constants };
