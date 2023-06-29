5a4768f0cca7eeaeb2cdc0bd25b64052b03e51a6
MatthewRenda <MatthewJRenda@Gmail.com>
2023-06-28 18:05:01 -0700

    cors

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ const corsOpts = {
 
 app.use(cors(corsOpts));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", constants.clientURL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
