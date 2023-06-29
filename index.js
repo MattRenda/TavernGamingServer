32d30fff1b0237ffd8cbb1bcdff845ed46c4d93a
MatthewRenda <MatthewJRenda@Gmail.com>
2023-06-28 17:42:44 -0700

    cors

--- index.js
+++ index.js
@@ -131,22 +131,22 @@ app.use((err, req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const corsOpts = {
-  origin: constants.clientURL,
+  // origin: constants.clientURL,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  origin: true,
-  withCredentials: true,
+  // allowedHeaders: ["Content-Type", "Authorization"],
+  origin: 'https://www.taverngaming.com',
+  // withCredentials: true,
 };
 
 app.use(cors(corsOpts));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(
   cookieSession({
     name: "session",
